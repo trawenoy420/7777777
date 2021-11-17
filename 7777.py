3b271cd4ec9d603329da4da56dcc01b500bd239b
trawenoy420 <mishgan070796@gmail.com>
2021-11-17 14:12:48 +0300

    dz7

---
+++ 7777.py
@@ -0,0 +1,88 @@
+
+################################ 2 ############################
+
+class DivisionByNull:
+    def __init__(self, div, den):
+        self.div = div
+        self.den = den
+
+    @staticmethod
+    def divide_by_null(div, den):
+        try:
+            return (div / den)
+        except:
+            return (f"Деление на ноль недопустимо")
+
+
+div = DivisionByNull(10, 100)
+print(DivisionByNull.divide_by_null(10, 0))
+print(DivisionByNull.divide_by_null(10, 0.1))
+print(div.divide_by_null(100, 0))
+
+
+############################### 3 ##################################
+
+
+
+class Error:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+        while True:
+            try:
+                val = int(input('Введите значения и нажимайте Enter - '))
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except:
+                print(f"Недопустимое значение - строка и булево")
+                y_or_n = input(f'Попробовать еще раз? Y/N ')
+
+                if y_or_n == 'Y' or y_or_n == 'y':
+                    print(try_except.my_input())
+                elif y_or_n == 'N' or y_or_n == 'n':
+                    return f'Вы вышли'
+                else:
+                    return f'Вы вышли'
+                
+try_except = Error(1)
+print(try_except.my_input())
+
+
+
+########################### 4 #########################################
+
+
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма z1 и z2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение z1 и z2 равно')
+        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'z = {self.a} + {self.b} * i'
+
+
+z_1 = ComplexNumber(1, -2)
+z_2 = ComplexNumber(3, 4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
+
+
+
+
+
+
+
+
+
+
